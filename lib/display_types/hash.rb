2516267fff178be3b3649434e0c008380d600c32
Philipp T <philipp@virtualop.org>
2012-09-01 23:42:42 +0200

    display type hash should support symbols as keys now

--- lib/display_types/hash.rb
+++ lib/display_types/hash.rb
@@ -1,7 +1,7 @@
 def format_hash_output(command, response)
   output = []
   
-  max_key_length = response.data.keys().sort { |a,b| a.length <=> b.length }.last.length
+  max_key_length = response.data.keys().sort { |a,b| a.to_s.length <=> b.to_s.length }.last.to_s.length
   
   response.data.keys.sort.each do |k|
     v = response.data[k]
