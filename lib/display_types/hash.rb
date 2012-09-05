d669ba708c80c38ee68d3e08305b2a52a4a91b17
Philipp T <philipp@virtualop.org>
2012-09-05 16:07:55 +0200

    more support for symbols in hashes

--- lib/display_types/hash.rb
+++ lib/display_types/hash.rb
@@ -3,7 +3,7 @@ def format_hash_output(command, response)
   
   max_key_length = response.data.keys().sort { |a,b| a.to_s.length <=> b.to_s.length }.last.to_s.length
   
-  response.data.keys.sort.each do |k|
+  response.data.keys.sort { |a,b| a.to_s <=> b.to_s }.each do |k|
     v = response.data[k]
     output << sprintf("  %-#{max_key_length + 5}s\t%s", k, v)
   end
